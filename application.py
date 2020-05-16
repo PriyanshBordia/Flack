d92151cf091daf79c23097dd280c69af9495c9f1
Priyansh Bordia <priyanshbordia1@gmail.com>
2020-05-16 19:10:38 +0530

    heroku files removed

--- application.py
+++ application.py
@@ -1,13 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, json, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
+
+
+@app.rote("")
