4c2b03ef6805bd88140cf4dd4baaa8e6cffd5fc0
Priyansh Bordia <priyanshbordia1@gmail.com>
2020-05-27 23:57:27 +0530

    removed redundant files

--- static/styles/js/chat_window.js
+++ static/styles/js/chat_window.js
@@ -1,85 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    channels = []
+	channels = []
 
-    document.querySelector('.create_channel').disabled = true;
+	document.querySelector('.create_channel').disabled = true;
 
-    document.querySelector('.new_msg').disabled = true;
+	document.querySelector('.new_msg').disabled = true;
 
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port)
+	var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port)
 
-    socket.on('connect', () => {
+	socket.on('connect', () => {
 
-        // Functionality on click on any channel name
-        document.querySelectorAll('#channels').forEach(channel => {
-            channel.onclick = () => {
-                const request = new XMLHttpRequest();
-                request.open('GET', `/${channel}`);
+		// Functionality on click on any channel name
+		document.querySelectorAll('#channels').forEach(channel => {
+			channel.onclick = () => {
+				const request = new XMLHttpRequest();
+				request.open('GET', `/${channel}`);
 
-                request.onload = () => {
-                    const response = request.responseText;;
-                    document.querySelector('#channel');
+				request.onload = () => {
+					const response = request.responseText;;
+					document.querySelector('#channel');
 
-                };
-            };
-        });
+				};
+			};
+		});
 
 // Channel Creation
-        document.querySelector('#channel_name').onkeyup = () => {
+		document.querySelector('#channel_name').onkeyup = () => {
 
-            if (document.querySelector('#channel_name').value.length > 0)
-            {
-                var x = document.querySelector('#channel_name').value).trim();
-                alert(`${x}`);
-                
-                if ((document.querySelector('#channel_name').value).trim() !== '')
-                    document.querySelector('.create_channel').disabled = false;
-            }
+			if (document.querySelector('#channel_name').value.length > 0)
+			{
+				var x = (document.querySelector('#channel_name').value).trim();
+				alert(`${x}`);
 
-            else
-                document.querySelector('.create_channel').disabled = true;
-        };
+				if (x !== '')
+					document.querySelector('.create_channel').disabled = false;
+			}
 
-        document.querySelector('#form_create_channel').onsubmit = () => {
+			else
+				document.querySelector('.create_channel').disabled = true;
+		};
 
-            const New_Channel = document.querySelector('#channel_name').value;
+		document.querySelector('#form_create_channel').onsubmit = () => {
 
-            const li = document.createElement('li');
-            li.innerHTML = New_Channel;
+			const New_Channel = document.querySelector('#channel_name').value;
 
-            document.querySelector('#channels-list').append(li);
+			const li = document.createElement('li');
+			li.innerHTML = New_Channel;
 
-            document.querySelector('.create_channel').disabled = true;
+			document.querySelector('#channels-list').append(li);
 
-            document.querySelector('#channel_name').value = '';
+			document.querySelector('.create_channel').disabled = true;
 
-            return false;
-        };
+			document.querySelector('#channel_name').value = '';
+
+			return false;
+		};
 
 // Messages
-        document.querySelector('#message_text').onkeyup = () => {
+		document.querySelector('#message_text').onkeyup = () => {
 
-            if (document.querySelector('#message_text').value.length > 0)
-                document.querySelector('.new_msg').disabled = false;
-            else
-                document.querySelector('.new_msg').disabled = true;
-        };
+			if (document.querySelector('#message_text').value.length > 0)
+				document.querySelector('.new_msg').disabled = false;
+			else
+				document.querySelector('.new_msg').disabled = true;
+		};
 
-        document.querySelector('#form_new_msg').onsubmit = () => {
+		document.querySelector('#form_new_msg').onsubmit = () => {
 
-            const New_message = document.querySelector('#message_text').value;
+			const New_message = document.querySelector('#message_text').value;
 
-            const li = document.createElement('li');
-            li.innerHTML = New_message;
+			const li = document.createElement('li');
+			li.innerHTML = New_message;
 
-            document.querySelector('#messages-list').append(li);
+			document.querySelector('#messages-list').append(li);
 
-            document.querySelector('.new_msg').disabled = true;
+			document.querySelector('.new_msg').disabled = true;
 
-            document.querySelector('#message_text').value = '';
+			document.querySelector('#message_text').value = '';
 
-            return false;
-        };
+			return false;
+		};
 
-    });
+	});
 });
